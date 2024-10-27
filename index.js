9dbabc600c4c2a6ce054809853a0747b98fe3945
Frank Njoroge <fnjoro29@gmail.com>
2024-10-27 13:37:14 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import getPostsLength, { getPosts } from "./postController.js";
+
+// const { generateRandomNumber, celciusToFahrenheight } = require("./utils");
+// console.log(`RandomNumber:`, generateRandomNumber());
+// console.log(`Fahrenheight:`, celciusToFahrenheight(0));
+
+console.log(getPostsLength());
